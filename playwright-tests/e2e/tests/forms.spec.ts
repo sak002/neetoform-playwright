5d2f1247b6cc9d1651e54b5788411f1d822db3fa
Samiksha <samikshadkamble@gmail.com>
2024-12-19 09:16:44 +0530

    Added tests to create and submit a form and verify submitted response

--- playwright-tests/e2e/tests/forms.spec.ts
+++ playwright-tests/e2e/tests/forms.spec.ts
@@ -3,14 +3,13 @@ import { test, expect, Page } from '@playwright/test';
 test.describe("Forms page", () => {
     test("should be able to add a new form", async ({ page, context }) => {
         let newPage: Page;
-        // test.setTimeout(120_000);
         await test.step("Step 1: Login to neetoform", async () => {
             await page.goto("https://neeto-form-web-playwright.neetodeployapp.com/login");
             await page.getByRole('button', { name: 'Login as Oliver' }).click();
         })
 
         await test.step("Step 2: Create a new form", async () => {
-            await page.getByRole('button', { name: 'Add new form' }).click();
+            await page.getByRole('button', { name: 'Add new form' }).click({ timeout: 10_000 });
             await page.getByText('Start from scratchA blank').click();
         })
 
@@ -24,7 +23,6 @@ test.describe("Forms page", () => {
         })
 
         await test.step("Step 5: Go to the published form", async () => {
-            
             [newPage] = await Promise.all([
                 page.waitForEvent('popup', { timeout: 10_000 }),
                 page.getByTestId('publish-preview-button').click()
@@ -51,16 +49,25 @@ test.describe("Forms page", () => {
             await newPage.getByRole('button', { name: 'Submit' }).click();
         })
 
-        await test.step("step 7: Fill in the form elements with valid values", async () => {
+        await test.step("Step 7: Fill in the form elements with valid values", async () => {
             await newPage.getByTestId('email-text-field').fill("oliver@example.com");
             await newPage.getByTestId('first-name-text-field').fill("Oliver");
             await newPage.getByTestId('last-name-text-field').fill("Smith");
-            const selectCountry = newPage.locator('.neeto-form-react-select__indicator').getByText('🇺🇸United States');
-            // await selectCountry.scrollIntoViewIfNeeded({ timeout: 30_000 });
-            await selectCountry.click({ timeout: 30_000 });
             await newPage.getByTestId('phone-number-input-field').fill("9087654321");
             await newPage.getByRole('button', { name: 'Submit' }).click();
         })
 
+        await test.step("Step 8: Check if thank you is visible", async () => {
+            await expect(newPage.getByRole('heading', { name: '🎉' })).toBeVisible();
+            await expect(newPage.getByRole('heading', { name: 'Thank You.' })).toBeVisible();
+            await expect(newPage.getByText('Your response has been')).toBeVisible();
+        })
+
+        await test.step("Step 9: Verify the submitted response", async () => {
+            await page.getByRole('link', { name: 'Submissions' }).click();
+            await expect(page.getByRole('cell', { name: 'oliver@example.com' })).toBeVisible();
+            await expect(page.getByRole('cell', { name: 'Oliver Smith' })).toBeVisible();
+            await expect(page.getByText('+1 908 765')).toBeVisible();
+        })
     })
 });
--- playwright-tests/e2e/tests/login.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-test.describe("Login page", () => {
-    test("should be able to login to neetoform", async ({ page }) => {
-        await page.goto("https://neeto-form-web-playwright.neetodeployapp.com/login");
-        await page.getByRole('button', { name: 'Login as Oliver' }).click();
-    })
-});

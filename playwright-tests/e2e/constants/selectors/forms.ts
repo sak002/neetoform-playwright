749395d24ee6469bff9f9a5e632dba0b7cfa1e55
Samiksha <samikshadkamble@gmail.com>
2024-12-20 14:19:45 +0530

    Added playwright tests to customize form's field elements

---
+++ playwright-tests/e2e/constants/selectors/forms.ts
@@ -0,0 +1,19 @@
+export const PUBLISHED_FORM_PAGE_SELECTORS = {
+    formHeading: 'Form Title',
+    emailLabel: 'Email address*',
+    fullNameLabel: 'Full name*',
+    phoneNumberLabel: 'Phone number*',
+    submitButtonName: 'Submit',
+    emailField: 'email-text-field',
+    phoneNumberField: 'phone-number-input-field',
+    firstNameField: 'first-name-text-field',
+    lastNameField: 'last-name-text-field',
+    singleChoiceOptions: "form-single-choice-option",
+
+}
+
+export const THANK_YOU_PAGE_SELECTORS = {
+    headingName1: '🎉',
+    headingName2: 'Thank You.',
+    responseText: 'Your response has been',
+}
---
+++ playwright-tests/e2e/constants/texts/form.ts
@@ -0,0 +1,12 @@
+export const FORM_TEXTS = {
+    defaultInvalidEmail: "oliver",
+    defaultInvalidPhoneNumber: "123",
+    defaultEmail: "oliver@example.com",
+    defaultFirstName: "Oliver",
+    defaultLastName: "Smith",
+    defaultPhoneNumber: "9087654321",
+    defaultCountryCode: "+1",
+    optionArray: ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6", "Option 7", "Option 8", "Option 9", "Option 10",],
+    singleChoiceQuestionText: "Single - demo field name",
+    multipleChoiceQuestionText: "Multiple - demo field name",
+}
